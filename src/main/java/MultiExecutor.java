7c7482109239ab5034842d98d270a7883cfaec1f
nivaldomonteiro <nushnivaldo@gmail.com>
2021-01-29 00:12:53 +0000

    Added multiexecutor

---
+++ src/main/java/MultiExecutor.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class MultiExecutor {
+    public static void main(String[] args) {
+        Test test = new Test();
+        MultiExecutor multiExecutor = new MultiExecutor(Arrays.asList(test));
+        multiExecutor.executeAll();
+    }
+
+    // Add any necessary member variables here
+    private List<Runnable> tasks;
+    /*
+     * @param tasks to executed concurrently
+     */
+    public MultiExecutor(List<Runnable> tasks) {
+        // Complete your code here
+        this.tasks = tasks;
+    }
+
+    /**
+     * Starts and executes all the tasks concurrently
+     */
+    public void executeAll() {
+        // complete your code here
+        for(Runnable task : tasks) {
+            Thread thread = new Thread(task);
+            thread.start();
+        }
+    }
+
+    private static class Test implements Runnable {
+         public void run() {
+             System.out.println("Hi there.");
+         }
+    }
+}
