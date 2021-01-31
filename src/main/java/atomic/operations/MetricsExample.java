3c4cfb396c0b4ba4d0faa2bc766f52cef38fce48
nivaldomonteiro <nushnivaldo@gmail.com>
2021-01-31 04:16:24 +0000

    Added atomic operations - closes #6

---
+++ src/main/java/atomic/operations/MetricsExample.java
@@ -0,0 +1,88 @@
+package atomic.operations;
+
+import java.util.Random;
+
+public class MetricsExample {
+
+    public static void main(String[] args) {
+        Metrics metrics = new Metrics();
+
+        BusinessLogic businessLogicThread1 = new BusinessLogic(metrics);
+
+        BusinessLogic businessLogicThread2 = new BusinessLogic(metrics);
+
+        MetricsPrinter metricsPrinter = new MetricsPrinter(metrics);
+
+        businessLogicThread1.start();
+        businessLogicThread2.start();
+        metricsPrinter.start();
+    }
+
+    public static class MetricsPrinter extends Thread {
+        private Metrics metrics;
+
+        public MetricsPrinter(Metrics metrics) {
+            this.metrics = metrics;
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                }
+
+                double currentAverage = metrics.getAverage();
+
+                System.out.println("Current Average is " + currentAverage);
+            }
+        }
+    }
+
+    public static class BusinessLogic extends Thread {
+        private Metrics metrics;
+        private Random random = new Random();
+
+        public BusinessLogic(Metrics metrics) {
+            this.metrics = metrics;
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                long start = System.currentTimeMillis();
+
+                try {
+                    Thread.sleep(random.nextInt(10));
+                } catch (InterruptedException e) {
+                }
+
+                long end = System.currentTimeMillis();
+
+                metrics.addSample(end - start);
+            }
+        }
+    }
+
+    private static class Metrics {
+
+        private long count = 0;
+        /**
+         * Beside knowing that assignment is atomic for primitive types
+         * it is not guaranteed for double
+         * so we add volatile keyword to make assignment operations atomic
+         */
+        private volatile double average = 0.0;
+
+        public synchronized void addSample(long sample) {
+            double currentSum = average * count;
+            count++;
+            average = (currentSum + sample) / count;
+        }
+
+        public double getAverage() {
+            return average;
+        }
+    }
+}
